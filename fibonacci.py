b64fdc081fb38e295d3c138006bd39ecd709353e
HamidOmarr <99067332@mydavinci.nl>
2021-11-10 09:27:04 +0100

    'next'

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci():
+    while fibonacci >= 1618:
+        fibonacci1 = 1
+        fibonacci2 = 1
+        result = (fibonacci1 + fibonacci2)
+        fibonacci3 = (result + fibonacci2 or fibonacci1)
+        fibonacci1 = 
+        print (fibonacci1)
+
+fibonacci()
